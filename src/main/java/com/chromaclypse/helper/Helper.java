d0f866980ca84ba4f03732e65842663dac613899
ezeiger92 <ezeiger92@gmail.com>
2020-01-01 15:36:22 -0500

    Reference new command API

--- src/main/java/com/chromaclypse/helper/Helper.java
+++ src/main/java/com/chromaclypse/helper/Helper.java
@@ -1,11 +1,12 @@
 package com.chromaclypse.helper;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.chromaclypse.api.command.CommandBase;
+import com.chromaclypse.api.command.Context;
 import com.chromaclypse.helper.guide.Guidebook;
 import com.chromaclypse.helper.guide.Guidebook.SerialSection;
 import com.chromaclypse.helper.guide.PageRedirect;
@@ -23,7 +24,16 @@ public class Helper extends JavaPlugin {
 	public void onEnable() {
 		reload();
 		
-		getCommand("assistant").setExecutor(this);
+		TabExecutor ast = new CommandBase()
+				.calls(this::helpCommand)
+				.with().arg("reload").calls(this::reloadCommand)
+				.with().arg("version").calls(CommandBase::pluginVersion)
+				.with().arg("search").calls(this::searchCommand)
+				.getCommand();
+		
+		getCommand("assistant").setExecutor(ast);
+		getCommand("assistant").setTabCompleter(ast);
+		
 		getCommand("guide").setExecutor(redirect);
 	}
 	
@@ -53,29 +63,27 @@ public class Helper extends JavaPlugin {
 		getServer().getPluginManager().registerEvents(closeCommand, this);
 	}
 	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length == 0) {
-			sender.sendMessage("[Assistant] Version " + getDescription().getVersion());
-			sender.sendMessage("[Assistant] /helper reload to reload");
-			return true;
+	public boolean helpCommand(Context context) {
+		if(context.Args().length > 0) {
+			context.Sender().sendMessage("[Assistant] Unknown arg \"" + context.GetArg(0) + "\"");
 		}
-		else if(args[0].equalsIgnoreCase("reload")) {
-			boolean hasPermission = sender.hasPermission("helper.reload");
-			
-			if(!hasPermission)
-				return false;
-			
-			sender.sendMessage("[Assistant] Reloading config");
+		
+		return CommandBase.pluginVersion(context);
+	}
+	
+	public boolean reloadCommand(Context context) {
+		if(context.Sender().hasPermission("helper.reload")) {
+			context.Sender().sendMessage("[Assistant] Reloading config");
 			reload();
 			return true;
 		}
-		else if(args[0].equalsIgnoreCase("search")) {
-			sender.sendMessage("[Assistant] Nothing to see here yet");
-			return true;
-		}
 		
-		sender.sendMessage("[Assistant] Unknown arg \"" + args[0] + "\"");
+		return false;
+	}
+	
+	public boolean searchCommand(Context context) {
+		context.Sender().sendMessage("[Assistant] Nothing to see here yet");
+		
 		return true;
 	}
 }
